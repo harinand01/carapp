f0a2b779daf4b07eaff69e881ff0be2247264cf6
hari <harinand084@gmail.com>
2026-04-02 21:12:22 +0530

    Switched to SQLite for deployment

--- carservice/settings.py
+++ carservice/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'carservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'carservice_db',
-        'USER': 'root',
-        'PASSWORD': 'mysql',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
